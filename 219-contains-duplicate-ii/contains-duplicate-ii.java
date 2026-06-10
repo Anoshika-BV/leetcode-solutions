eeb816d06a810d8a5551ba7c68a4def86d4aed10
Anoshika B V <anoshika2004@gmail.com>
2026-06-10 13:55:01 +0530

    Time: 23 ms (80.31%) | Memory: 90.7 MB (25.91%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+    //test
+    class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+                return true;
+            }
+            seen.put(val, i);
+        }
+
+        return false;        
+    }
+}
