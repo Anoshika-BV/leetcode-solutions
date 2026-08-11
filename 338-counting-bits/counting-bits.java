afecada1eda890d8dac70bafaf08b4d5b392e289
Anoshika B V <anoshika2004@gmail.com>
2026-08-11 12:16:49 +0530

    Time: 7 ms (13.26%) | Memory: 48.6 MB (70.60%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countBits(int n) {
+        //test
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            int num = i;
+            int count = 0;
+
+            while (num > 0) {
+                count += num % 2;
+                num = num / 2;
+            }
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+}
